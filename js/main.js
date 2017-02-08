021c073677f3978b0b70a10d508a22ebc81d4250
m-woo <mark.chi.woo@gmail.com>
2017-02-07 19:53:29 -0800

    functional submit and clear buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,37 @@
 console.log('hi')
+
+// functional submit/input field
+
+// function todoList() {
+// 	var item = $('#toInput').val();
+// 	var text = document.createTextNode(item)
+// 	var newItem = document.createElement('li')
+// 	newItem.appendChild(text)
+// 	document.getElementById('todoList').appendChild(newItem)
+// }
+
+// -------------------
+
+// get text from input
+
+// var item = $('#toInput').val();
+
+$('#button').click(function() {
+	item = $('#toInput').val();
+	addNewItem(item);
+});
+
+function addNewItem(item){
+	$('#todoList').append('<li>' + item + '</li>');
+};
+
+// adding delete button functionality
+
+$('#clear').click(function() {
+	$('#todoList').remove();
+});
+
+// $('li').click(function() {
+// 	$('li').remove();
+// });
+
